6f1f4949091806bc899a879c0cf5e682ab86520d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-07 16:49:10 +0530

    Add files via upload

---
+++ src/main/java/EbillApp.java
@@ -0,0 +1,34 @@
+
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.platform.commons.JUnitException;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+
+
+
+
+public class EbillApp {
+	
+
+	public static void main(String[] args) {
+		
+		Result result = JUnitCore.runClasses(EbillTest.class);
+        
+        if (result.getFailureCount() == 0)
+            System.out.println("There are No Failures...\n Test Passed...");
+        else {
+            for(Failure failure: result.getFailures()) {
+                 System.out.println("The Test execution failed...\n" + failure.getMessage());
+            }
+        }
+        System.out.println("Result" + result.wasSuccessful());
+
+	}
+}
